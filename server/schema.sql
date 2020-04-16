f3302b7bc45b4ec0bff7628ee43293b1c7becec2
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-04-15 17:30:11 -0700

    Started Sprint with Danny

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,20 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE username (
+  usernameID INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20),
+  PRIMARY KEY (usernameID)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  messages VARCHAR(140),
+  FOREIGN KEY (messages) REFERENCES username(usernameID)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
